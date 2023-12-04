3f372c490382821d0b63c09ad49bb97c33c81231
imhojiakbar <imhojiakbar@gmail.com>
2023-12-04 15:44:46 +0500

    new task id sone

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-git
+from pprint import pprint
+import os
+import httpx
+url = "https://jsonplaceholder.typicode.com/todos"
+response = httpx.get(url=url)
+data = response.json()
+
+os.mkdir("todos")
+os.chdir("todos")
+for todo in data:
+    with open(f"{todo['id']}.txt", "w") as f:
+        f.write(f"Title: {todo['title']}\n")
+        f.write(f"User Id: {todo['userId']}\n")
+
